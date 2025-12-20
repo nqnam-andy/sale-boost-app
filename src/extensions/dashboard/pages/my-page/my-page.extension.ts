0d0b8e9086a76f06e0276a86a2afce506ba9176a
Andy <namnq@tigren.com>
2025-12-20 09:59:53 +0700

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '99bfff6a-6742-4375-80f5-87fa60718248',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
